b2f3e2355c332e8900d76f0ea8c68300ef1ab15f
Luiz Fabiano de Souza <luizfabianods04@gmail.com>
2025-02-28 16:41:15 -0300

    Big card created

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'menu-title',
+  imports: [],
+  templateUrl: './menu-title.component.html',
+  styleUrl: './menu-title.component.css'
+})
+export class MenuTitleComponent {
+
+}
